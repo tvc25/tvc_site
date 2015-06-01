6c7c7b5ec20da07e2385bee0f7fa52ed86c5675a
Tobia Cassandro <tobiacassandro@gmail.com>
2015-06-01 18:34:24 +0100

    added CSS files and all other files connected

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def about_me
+      @email = "tobiacassandro@gmail.com"
+      @address = "17 Woronzow Road, London, NW8 6BA"
+  end 
+  
+  def work_experience
+  end
+  
+  def interests 
+  end
+  
+  def music
+  end
+  
+  def football_team
+  end
+
+  def home_page
+  end
+end
